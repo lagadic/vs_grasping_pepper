678b1018695eb9025c80c6fcb624ba24ef89035e
jokla <giovanni.claudio@inria.fr>
2017-01-17 17:01:53 +0100

    First working version

--- include/vs_grasping_pepper.h
+++ include/vs_grasping_pepper.h
@@ -5,6 +5,7 @@
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/CameraInfo.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/TransformStamped.h>
 #include <std_msgs/Int8.h>
 
 #include <visp3/core/vpHomogeneousMatrix.h>
@@ -29,9 +30,10 @@ public:
   vs_grasping_pepper(ros::NodeHandle &nh);
   ~vs_grasping_pepper();
   void computeControlLaw();
+  void saveOffset();
   void spin();
   void getActualPoseCb(const geometry_msgs::PoseStampedConstPtr &msg);
-  void getDesiredPoseCb(const geometry_msgs::PoseStampedConstPtr &msg);
+  void getDesiredPoseCb(const geometry_msgs::TransformStampedConstPtr &msg);
   void getStatusPoseHandCb(const std_msgs::Int8::ConstPtr &status);
   void getStatusPoseDesiredCb(const std_msgs::Int8::ConstPtr &status);
   void getCameraInfoCb(const sensor_msgs::CameraInfoConstPtr &msg);
@@ -82,6 +84,8 @@ protected:
   vpColVector m_q2_dot;
   vpCameraParameters m_cam;
 
+  std::string m_offsetFileName;
+
   double m_servo_time_init;
   int m_statusPoseHand;
   int m_statusPoseDesired;
@@ -89,6 +93,7 @@ protected:
   vpHomogeneousMatrix m_cMh;
   vpHomogeneousMatrix m_cMdh;
   vpHomogeneousMatrix m_eMh;
+  vpHomogeneousMatrix m_offset;
 
   //conditions
   bool m_cMh_isInitialized;
--- src/vs_grasping_pepper.cpp
+++ src/vs_grasping_pepper.cpp
@@ -22,6 +22,7 @@ vs_grasping_pepper::vs_grasping_pepper(ros::NodeHandle &nh): m_cam(),  m_camInfo
   m_statusPoseHand = 0;
   m_statusPoseDesired = 0;
   m_servo_time_init = 0;
+  m_offset.setIdentity();
 
   n.param( "frequency", freq, 100);
   n.param<std::string>("ip", m_ip, "131.254.10.126");
@@ -32,8 +33,16 @@ vs_grasping_pepper::vs_grasping_pepper(ros::NodeHandle &nh): m_cam(),  m_camInfo
   n.param<std::string>("statusPoseHandTopicName", statusPoseHandTopicName, "/pepper_hand_pose/status");
   n.param<std::string>("opt_arm", m_opt_arm, "right");
   n.param<std::string>("eMh_pepper_path", m_eMh_pepper_path, "");
+  n.param<std::string>("offsetFileName", m_offsetFileName, "pose.xml");
   n.param("statusPoseDesired_isEnable", m_statusPoseDesired_isEnable, false );
   n.param("mode", m_mode, 0);
+
+  if (m_mode == 0)
+    std::cout << "Mode 0: Visual servoing mode" << std::endl;
+
+  if (m_mode == 2)
+    std::cout << "Mode 2: save offset from object to hand" << std::endl;
+
 
   if (m_mode < 0 || m_mode > 3)
   {
@@ -50,7 +59,7 @@ vs_grasping_pepper::vs_grasping_pepper(ros::NodeHandle &nh): m_cam(),  m_camInfo
   else
     m_statusPoseDesired = 1;
 
-  desiredPoseSub = n.subscribe( desiredPoseTopicName, 1, (boost::function < void(const geometry_msgs::PoseStampedConstPtr&)>) boost::bind( &vs_grasping_pepper::getDesiredPoseCb, this, _1 ));
+  desiredPoseSub = n.subscribe( desiredPoseTopicName, 1, (boost::function < void(const geometry_msgs::TransformStampedConstPtr&)>) boost::bind( &vs_grasping_pepper::getDesiredPoseCb, this, _1 ));
   actualPoseSub = n.subscribe( actualPoseTopicName, 1, (boost::function < void(const geometry_msgs::PoseStampedConstPtr &)>) boost::bind( &vs_grasping_pepper::getActualPoseCb, this, _1 ));
   statusPoseHandSub = n.subscribe ( statusPoseHandTopicName, 1, (boost::function < void(const std_msgs::Int8::ConstPtr  &)>) boost::bind( &vs_grasping_pepper::getStatusPoseHandCb, this, _1 ));
   m_cameraInfoSub = n.subscribe( m_cameraInfoName, 1, (boost::function < void(const sensor_msgs::CameraInfoConstPtr & )>) boost::bind( &vs_grasping_pepper::getCameraInfoCb, this, _1 ));
@@ -71,6 +80,18 @@ vs_grasping_pepper::vs_grasping_pepper(ros::NodeHandle &nh): m_cam(),  m_camInfo
   }
   else
     std::cout << "Homogeneous matrix " << name_transform <<": " << std::endl << m_eMh << std::endl;
+
+  if (m_mode == 0)
+  {
+    std::string name_transform_offset = "dhMoffset_"+ m_chain_name;
+
+    if (pm.parse(m_offset, m_offsetFileName, name_transform_offset) != vpXmlParserHomogeneousMatrix::SEQUENCE_OK) {
+      std::cout << "Cannot find the homogeneous matrix named " << name_transform_offset << "." << std::endl;
+      ros::shutdown();
+    }
+    else
+      std::cout << "Homogeneous matrix " << name_transform_offset <<": " << std::endl << m_eMh << std::endl;
+  }
 
   // Init display
   I.resize(480, 640);
@@ -136,16 +157,20 @@ void vs_grasping_pepper::spin()
 
     vpMouseButton::vpMouseButtonType button;
     bool ret = vpDisplay::getClick(I, button, false);
-
-    if (ret && button == vpMouseButton::button2)
+    if (m_mode == 1)
     {
-      m_cMdh = m_cMh;
-      m_cMdh_isInitialized = true;
-      std::cout << "Desired Pose saved" << std::endl;
+      if (ret && button == vpMouseButton::button2)
+      {
+        m_cMdh = m_cMh;
+        m_cMdh_isInitialized = true;
+        std::cout << "Desired Pose saved" << std::endl;
+      }
     }
     if (m_cMdh_isInitialized)
+    {
       vpDisplay::displayFrame(I, m_cMdh, m_cam, 0.06, vpColor::red);
-
+      vpDisplay::displayFrame(I, m_cMdh*m_offset, m_cam, 0.06, vpColor::cyan);
+    }
     if (m_statusPoseHand)
       vpDisplay::displayFrame(I, m_cMh, m_cam, 0.06, vpColor::none);
 
@@ -155,10 +180,21 @@ void vs_grasping_pepper::spin()
       m_servo_enabled = !m_servo_enabled;
     }
 
-    if (m_servo_enabled)
+    if (m_mode == 2)
     {
-      vpDisplay::displayText(I, 30, 30, "Servo enabled", vpColor::green);
-      this->computeControlLaw();
+      vpDisplay::displayText(I, 30, 30, "Press middle button to save the offset", vpColor::green);
+      if (ret && button == vpMouseButton::button2)
+      {
+        this->saveOffset();
+      }
+    }
+    else if (m_mode == 0 || m_mode == 1 )
+    {
+      if (m_servo_enabled)
+      {
+        vpDisplay::displayText(I, 30, 30, "Servo enabled", vpColor::green);
+        this->computeControlLaw();
+      }
     }
 
     ret = false;
@@ -168,6 +204,38 @@ void vs_grasping_pepper::spin()
     loop_rate.sleep();
   }
 }
+
+
+void vs_grasping_pepper::saveOffset()
+{
+
+  vpHomogeneousMatrix dhMoffset = m_cMdh.inverse() * m_cMh;
+  vpXmlParserHomogeneousMatrix xml;
+  std::string path = "/tmp/dhMoffset_pepper.xml";
+
+  if (m_cMh_isInitialized && m_cMdh_isInitialized && m_statusPoseHand && m_statusPoseDesired )
+  {
+    ROS_INFO_STREAM("cMdh = " << m_cMdh << "\n cMh = " << m_cMh << "\n dhMh = " << dhMoffset);
+    if( xml.save(dhMoffset, path.c_str(),"dhMoffset") == vpXmlParserHomogeneousMatrix::SEQUENCE_OK )
+      std::cout << "Pose between the hand and the object successfully saved in \"" << path << "\"" << std::endl;
+    else {
+      std::cout << "Failed to save the pose in \"" << path << "\"" << std::endl;
+      std::cout << "A file with the same name exists. Remove it to be able to save the parameters." << std::endl;
+    }
+    ros::shutdown();
+  }
+  else
+  {
+    std::cout << "m_cMh_isInitialized: " << m_cMh_isInitialized << std::endl
+              << "m_cMdh_isInitialized: " << m_cMdh_isInitialized << std::endl
+              << "m_statusPoseHand: " << m_statusPoseHand << std::endl
+              << "m_statusPoseDesired: " << m_statusPoseDesired << std::endl;
+  }
+
+
+}
+
+
 
 void vs_grasping_pepper::computeControlLaw()
 {
@@ -181,10 +249,10 @@ void vs_grasping_pepper::computeControlLaw()
       first_time = false;
     }
 
-    vpAdaptiveGain lambda(0.8, 0.05, 8);
+    vpAdaptiveGain lambda(0.9, 0.1, 8);
     m_servo_arm.setLambda(lambda);
     m_servo_arm.set_eJe(robot.get_eJe(m_chain_name));
-    m_servo_arm.setCurrentFeature(m_cMdh.inverse() * m_cMh) ;
+    m_servo_arm.setCurrentFeature(m_offset.inverse() * m_cMdh.inverse() * m_cMh) ;
     // Create twist matrix from target Frame to Arm end-effector (WristPitch)
     vpVelocityTwistMatrix oVe_LArm(m_eMh);
     m_servo_arm.m_task.set_cVe(oVe_LArm);
@@ -217,9 +285,9 @@ void vs_grasping_pepper::publishCmdVel(const vpColVector &q)
 }
 
 
-void vs_grasping_pepper::getDesiredPoseCb(const geometry_msgs::PoseStamped::ConstPtr &desiredPose)
+void vs_grasping_pepper::getDesiredPoseCb(const geometry_msgs::TransformStamped::ConstPtr &desiredPose)
 {
-  m_cMdh = visp_bridge::toVispHomogeneousMatrix(desiredPose->pose);
+  m_cMdh = visp_bridge::toVispHomogeneousMatrix(desiredPose->transform);
 
   if ( !m_cMdh_isInitialized )
   {
@@ -243,7 +311,7 @@ void vs_grasping_pepper::getActualPoseCb(const geometry_msgs::PoseStamped::Const
 void vs_grasping_pepper::getStatusPoseHandCb(const std_msgs::Int8::ConstPtr  &status)
 {
   m_statusPoseHand = status->data;
-  std::cout << "Status_pose_hand" << m_statusPoseHand << std::endl;
+  // std::cout << "Status_pose_hand" << m_statusPoseHand << std::endl;
 }
 
 void vs_grasping_pepper::getStatusPoseDesiredCb(const std_msgs::Int8::ConstPtr  &status)
